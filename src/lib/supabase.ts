101c14d1d83ccea7693b97e90aeb46c4e33285be
aditya-baraik <aditya.c.baraik@gmail.com>
2026-07-30 23:34:47 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,9 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = 'https://azggwkbzlihmquzpvdmf.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_nit6Giy4-k4zOjVg0qLS9g_RYjz2CzJ';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
